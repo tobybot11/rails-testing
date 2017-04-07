34c396a530b72390b0edba9eb73ea703a175f99c
Tobias Ford <toby.ford@pobox.com>
2017-04-07 13:40:29 -0400

    added the initial devise integrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :redirect_url
+
+  def redirect_url
+    return new_user_session_path unless user_signed_in?
+    root_path
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-  root to: "users#index"
+  # didn't realize this resource call was no longer needed?
+  #  resources :users
+
+  unauthenticated do
+       root to: redirect('/login.html') 
+  end
+
+  authenticated do
+      root to: redirect('/logout.html')
+  end
+#  root to: "users#index" -> works
+#  root to: new_user_session_path -> fail?
+#  root to: "devise/sessions#new" -> fail?
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
