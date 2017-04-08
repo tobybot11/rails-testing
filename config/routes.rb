f5553596c83433122e9c4e1dbae6158550360a94
Tobias Ford <toby.ford@pobox.com>
2017-04-08 07:57:11 -0400

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -16,19 +16,6 @@ Rails.application.routes.draw do
     }
     resources :users
 
-    #unauthenticated do
-    #  root to: 'auth/sessions#new'
-    #end
-
-    #authenticated do
-    #  root to: 'users#index'
-    #end
     root to: 'users#index'
-    #  root to: redirect('/login.html') 
-    # redirect('/logout.html')
-    #  root to: "users#index" -> works
-    #  root to: new_user_session_path -> fail?
-    #  root to: "devise/sessions#new" -> fail?
-
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
