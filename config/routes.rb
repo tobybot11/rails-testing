ccdac99d016888d7e71a87399fbaf890072a5b4e
Tobias Ford <toby.ford@pobox.com>
2017-04-08 07:44:57 -0400

    figured out how to control devise routes .. using devise_for :users more completely

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,33 @@ Rails.application.routes.draw do
   # devise_for :users
   # didn't realize this resource call was no longer needed?
 
-  devise_for :users, class_name: User, controllers: {
-    sessions: 'auth/sessions'
-  }
-  resources :users
+  devise_for :users, 
+    path: "auth",
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'join_now',
+      confirmation: 'verification',
+      registration: 'register',
+    },
+    controllers: {
+      sessions: 'auth/sessions' 
+    }
+    resources :users
 
-  #unauthenticated do
-  #  root to: 'auth/sessions#new'
-  #end
+    #unauthenticated do
+    #  root to: 'auth/sessions#new'
+    #end
 
-  #authenticated do
-  #  root to: 'users#index'
-  #end
-  root to: 'users#index'
-#  root to: redirect('/login.html') 
-      # redirect('/logout.html')
-#  root to: "users#index" -> works
-#  root to: new_user_session_path -> fail?
-#  root to: "devise/sessions#new" -> fail?
+    #authenticated do
+    #  root to: 'users#index'
+    #end
+    root to: 'users#index'
+    #  root to: redirect('/login.html') 
+    # redirect('/logout.html')
+    #  root to: "users#index" -> works
+    #  root to: new_user_session_path -> fail?
+    #  root to: "devise/sessions#new" -> fail?
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
